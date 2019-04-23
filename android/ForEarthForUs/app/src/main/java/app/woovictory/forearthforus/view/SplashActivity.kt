0ca812df0503f61b83392b577f7b34573d52b459
Woo <jhsw0375@gmail.com>
2019-04-23 23:24:42 +0900

    <android> add splash activity.

---
+++ android/ForEarthForUs/app/src/main/java/app/woovictory/forearthforus/view/SplashActivity.kt
@@ -0,0 +1,13 @@
+package app.woovictory.forearthforus.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.woovictory.forearthforus.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
