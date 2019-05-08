ca283326fdee17ec10c10172f2a227195ceba436
Woo <jhsw0375@gmail.com>
2019-05-08 11:19:15 +0900

    <Android> BaseViewModel, BaseActivity 작성

--- android/ForEarthForUs/app/src/main/java/app/woovictory/forearthforus/MainActivity.kt
+++ android/ForEarthForUs/app/src/main/java/app/woovictory/forearthforus/MainActivity.kt
@@ -2,16 +2,25 @@ package app.woovictory.forearthforus
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import app.woovictory.forearthforus.base.BaseActivity
+import app.woovictory.forearthforus.databinding.ActivityMainBinding
 import app.woovictory.forearthforus.view.mission.MissionFragment
 import app.woovictory.forearthforus.view.mypage.MyPageFragment
+import app.woovictory.forearthforus.vm.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    override val viewModel: MainViewModel = MainViewModel()
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.navigation_main -> {
             toast("ddd?")
@@ -36,11 +45,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         mainBottomNavigation.setOnNavigationItemSelectedListener(this@MainActivity)
+    }
+
+    override fun initStartView() {
 
     }
 
+    override fun initDataBinding() {
+
+    }
 
     private fun changeFragment(fragment: Fragment) {
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
