38d6de448f38596494ea9a8dbdecdd19d605899b
Jaehyun Cho <looksgoood12@gmail.com>
2019-05-19 00:28:17 +0900

    <Backend> Delete image after delete Prefer model.

--- backend/ForEarthForUs_backend/api/models.py
+++ backend/ForEarthForUs_backend/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_delete
+from .utils.media_clean import file_cleanup
 
 userModel = get_user_model()
 
@@ -16,3 +18,5 @@ class Prefer(models.Model):
 class UserPrefer(models.Model):
     user = models.ForeignKey(userModel, related_name='prefer', on_delete=models.CASCADE)
     prefer = models.OneToOneField(Prefer, on_delete=models.CASCADE)
+
+post_delete.connect(file_cleanup, sender=Prefer, dispatch_uid="prefer.file_cleanup")
