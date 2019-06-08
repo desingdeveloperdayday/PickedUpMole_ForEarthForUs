2dcb5a22de7398a61ab3c319af92a18299abfb79
Jaehyun Cho <looksgoood12@gmail.com>
2019-06-08 21:16:47 +0900

    <Backend> remove images when clean DB.

--- backend/ForEarthForUs_backend/clean_db.sh
+++ backend/ForEarthForUs_backend/clean_db.sh
@@ -3,6 +3,7 @@
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 rm db.sqlite3
+rm -rf media/images
 
 python manage.py makemigrations
 python manage.py migrate
