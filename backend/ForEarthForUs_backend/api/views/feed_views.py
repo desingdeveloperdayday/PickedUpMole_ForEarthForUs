891e06f567b8f8428a07688e2585d7a5059bd9d3
Jaehyun Cho <looksgoood12@gmail.com>
2019-06-24 23:17:26 +0900

    <Backend> earthLevel 로직 추가

--- backend/ForEarthForUs_backend/api/views/feed_views.py
+++ backend/ForEarthForUs_backend/api/views/feed_views.py
@@ -7,6 +7,8 @@ from api.permission import *
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime, timedelta, time
+from django.contrib.auth import get_user_model
+from api.utils.earth_check import increase_earthLevel, decrease_earthLevel
 
 STATUS_PROGRESS = 'progress'
 STATUS_COMPLETE = 'complete'
@@ -67,12 +69,26 @@ class FeedViewSet(viewsets.ModelViewSet):
         instance = self.queryset.get(pk=pk)
         if instance is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+        firstComplete = False
+        if not instance.complete:
+            firstComplete = True
+        
         request.data["complete"] = True
         serializer = FeedUpdateSerializer(
             instance, data=request.data,
             partial=True, context={"request": request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
+        # Earth level uypdate logic
+        if firstComplete:
+            userModel = get_user_model()
+            userInstance = userModel.objects.get(email=request.user.email)
+            newEarthLevel = increase_earthLevel(userInstance.earthLevel)
+            if newEarthLevel is not userInstance.earthLevel:
+                userInstance.earthLevel = newEarthLevel
+                userInstance.save()
 
         read_serializer = FeedReadSerializer(instance, context={"request": request})
         return Response(read_serializer.data, status=status.HTTP_202_ACCEPTED)
