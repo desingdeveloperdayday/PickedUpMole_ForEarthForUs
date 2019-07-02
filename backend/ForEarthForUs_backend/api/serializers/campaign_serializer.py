2ad4d3be035ae3cfd62f3f0e20159f675f4395ad
Jaehyun Cho <looksgoood12@gmail.com>
2019-07-02 22:11:48 +0900

    <Backend> scrap field 추가
    campaign, article에 scrap(True/False) field를
    추가하여 scrap 된 item인지 아닌지 판단하도록 추가

--- backend/ForEarthForUs_backend/api/serializers/campaign_serializer.py
+++ backend/ForEarthForUs_backend/api/serializers/campaign_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from api.models.campaign_models import Campaign
 from api.utils.validate_svg import validate_image_extension
+from api.models.scrap_models import Scrap
 
 class CampaignSerializer(serializers.HyperlinkedModelSerializer):
     image = serializers.FileField(max_length=None, use_url=True,
@@ -9,3 +10,23 @@ class CampaignSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Campaign
         fields = ('id', 'title', 'subTitle', 'image', 'link')
+
+    def to_representation(self, instance):
+        data = super(CampaignSerializer, self).to_representation(instance)
+        data['scrap'] = update_status(self.context['request'].user.id, data['id'])
+        return data
+
+def update_status(userId, campaignId):
+    scrap = Scrap.objects.filter(
+                user=userId,
+                kind=1,
+                campaign=campaignId
+            )
+
+    val = False
+    if scrap:
+        val = True
+    else:
+        val = False
+
+    return val
